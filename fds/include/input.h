80fdaf43e29837a214d5559c450e59d0ea7b902c
Daniel Kelshaw <daniel.j.kelshaw@gmail.com>
2020-11-10 01:02:54 +0000

    Templated UAVClass
    Templated a `UAVClass` and provided a method to read `JSON` from file.
    
    Resolves: #6

--- fds/include/input.h
+++ fds/include/input.h
@@ -2,10 +2,15 @@
 #define INPUT_H
 
 #include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
 #include <cmath>
 #include <vector>
 #include <array>
 #include <Eigen/Dense>
+
+const double rho = 1.225;
+const double g0  = 9.81;
 
 enum eQuaternion {e_e0, e_e1, e_e2, e_e3};
 enum eEuler {e_phi, e_theta, e_psi};
--- fds/src/main.cpp
+++ fds/src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include "../include/uav.h"
 
 int main() {
 
 	std::cout << "Flight Dynamics Simulation" << std::endl;
+
+	UAVClass uav;
+
+	std::cout << "DONE" << std::endl;
 
 	return 0;
 }
